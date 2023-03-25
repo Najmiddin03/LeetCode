756d6346d51da33b163612097baac91ca4bc7e8c
Turabek07 <nazarmatovn03@gmail.com>
2023-03-25 20:49:34 +0100

    longestSubstring

---
+++ src/longestSubstring/Main.java
@@ -0,0 +1,35 @@
+package longestSubstring;
+
+import java.util.HashMap;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(longestSubstring(""));
+	}
+
+	public static int longestSubstring(String s) {
+		HashMap<Character, Boolean> h = new HashMap<>();
+		int count = 0;
+		int max = 0;
+		for (int j = 0; j < s.length(); j++) {
+			for (int i = j; i < s.length(); i++) {
+				if (h.containsKey(s.charAt(i))) {
+					h.clear();
+					if (count > max) {
+						max = count;
+					}
+					count = 0;
+					break;
+				}
+				h.put(s.charAt(i), true);
+				count++;
+			}
+		}
+		if (count > max) {
+			max = count;
+		}
+		return max;
+	}
+
+}
