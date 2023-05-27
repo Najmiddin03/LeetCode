6d264ff3a59ef5c4c53eb6c567b3c79f481bcae1
Turabek07 <nazarmatovn03@gmail.com>
2023-05-27 20:01:11 +0200

    top150

--- src/topInterview150/H_index.java
+++ src/topInterview150/H_index.java
@@ -4,15 +4,17 @@ import java.util.Arrays;
 
 public class H_index {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//	Given an array of integers citations where citations[i] is the number of citations a researcher received 
+//	for their ith paper, return the researcher's h-index.
+//	According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h 
+//	such that the given researcher has published at least h papers that have each been cited at least h times.
 
+	public static void main(String[] args) {
+		int arr[] = { 4, 2, 3, 6, 9, 8 };
+		System.out.println(hIndex(arr));
 	}
 
 	static int hIndex(int[] citations) {
-//		if (citations.length == 1) {
-//			return 1;
-//		}
 		int ans = 0;
 		Arrays.sort(citations);
 		for (int i = citations.length - 1; i >= 0; i--) {
